bb2fff053877a0b839c099dfcecf6024c7669983
dr3dful1 <karmstr2@gmail.com>
2020-04-28 16:11:59 -0700

    initial

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const BlogPost= require('./models/BlogPost');
 mongoose.connect('mongodb://localhost/my_database',{useNewUrlParser:true})
 
 BlogPost.create({
-    title: ' The   Mythbusters guide.weevbnvbwrhgg...',
+    title: ' The   Mythbusters guide.wgfhfgeevbnvbwrwererwhgg...',
     body: ' if you ever been here a long time, you...'
 }, (error, blogpost)=>{
     console.log(error,blogpost)
