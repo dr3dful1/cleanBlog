79bbd849fc841900e884bdfea9e5bc5d1c3eebe3
dr3dful1 <karmstr2@gmail.com>
2020-04-30 11:15:32 -0700

    Post title and body connected

--- index.js
+++ index.js
@@ -5,14 +5,29 @@ const path= require('path');
 app.use(express.static('public'));
 const ejs= require('ejs');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_database',{useNewUrlParser:true})
+
+
+mongoose.connect('mongodb://localhost/my_database',{useUnifiedTopology: true, useNewUrlParser:true}).then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log(`DB Connection Error: ${err.message}`);
+});
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine','ejs');
+const BlogPost = require('./models/BlogPost.js');
+
 
 app.listen(4000,()=>{
     console.log('app listening on port 4000');
-    app.get('/',(req,res)=>{
-        
-        res.render('index');
+
+    app.get('/',async (req,res)=>{
+        const blogposts = await BlogPost.find({})
+        res.render('index',{
+            blogposts
+        });
     });
     app.get('/about',(req,res)=>{
         
@@ -22,10 +37,25 @@ app.listen(4000,()=>{
        
         res.render('contact');
     })
-    app.get('/post',(req,res)=>{
-         
-        res.render('post');
+    app.get('/post/:id',async (req,res)=>{
+         const blogpost = await BlogPost.findById(req.params.id)
+        res.render('post',{
+            blogpost
+        })
     })
+    app.get('/posts/new',(req,res)=>{
+        res.render('create')
+    })
+
+    app.post('/posts/store',(req,res)=>{
+        BlogPost.create(req.body,(error,blogpost)=>{
+            res.redirect('/')
+
+        })
+    })
+
+    
+
 
 
 
--- test.js
+++ test.js
@@ -2,21 +2,37 @@ const mongoose= require('mongoose');
 const BlogPost= require('./models/BlogPost');
 mongoose.connect('mongodb://localhost/my_database',{useNewUrlParser:true})
 
-BlogPost.create({
-    title: ' The   Mythbusters guirtyryfghhgfghde.wgfhfgeevbnvbwrwererwhgg...',
-    body: ' if you ever been here a long time, you...'
-}, (error, blogpost)=>{
-    console.log(error,blogpost)
-})
+// BlogPost.create({
+//     title: ' The   Mythbusters guirtyryfghhgfghde.wgfhfgeevbnvbwrwererwhgg...',
+//     body: ' if you ever been here a long time, you...'
+// }, (error, blogpost)=>{
+//     console.log(error,blogpost)
+// })
 
-BlogPost.create({
-    title: '  bghde.wgfhfgeevbnvbwrwererwhgg...',
-    body: '  en here a long time, you...'
-}, (error, blogpost)=>{
-    console.log(error,blogpost)
-})
+// BlogPost.create({
+//     title: '  bghde.wgfhfgeevbnvbwrwererwhgg...',
+//     body: '  en here a long time, you...'
+// }, (error, blogpost)=>{
+//     console.log(error,blogpost)
+// })
 
-BlogPost.find({}, (error, blogpost)=>{
-    console.log(error,blogpost)
-})
+// BlogPost.create({
+//     title: ' The The  bghde.wgfhfgeevbnvbwrwererwhgg...',
+//     body: '  en here a long time, you...'
+// }, (error, blogpost)=>{
+//     console.log(error,blogpost)
+// })
 
+// BlogPost.find({
+//     title:/the/
+// }, (error, blogpost)=>{
+//     console.log(error,blogpost)
+// })
+
+ var id="5ea9bb5fa8dab43b0013cd3c";
+//  BlogPost.findById(id,(error,blogspot)=>{
+//      console.log(error,blogspot)
+//  })
+BlogPost.findByIdAndDelete(id,(error,blogspot)=>{
+    console.log(error,blogspot)
+})
